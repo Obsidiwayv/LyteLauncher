fa19934eb86739119beb6f4c1952cb851861019e
Wayvshock <pizza5803@gmail.com>
2026-01-14 04:38:45 -0500

    Add StellarStrap

---
+++ StellarStrap/RobloxSetupJSON.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace StellarStrap
+{
+    public class RobloxWindowsPlayerManifest
+    {
+        [JsonPropertyName("version")]
+        public string Version { get; }
+
+        [JsonPropertyName("clientVersionUpload")]
+        public string ClientVersionUpload { get; }
+
+        [JsonPropertyName("bootstrapperVersion")]
+        public string BootstrapperVersion { get; }
+    }
+}
